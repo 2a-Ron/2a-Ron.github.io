37de3e139046ec3c34a979e31e24fe6fa4377d6d
2a-Ron <ematrix_frag@hotmail.com>
2019-02-02 13:38:20 -0800

    my first commit

---
+++ js/app.js
@@ -0,0 +1,33 @@
+'use strict';
+/*
+var charName = prompt('What is your charater name?');
+console.log(charName);
+
+var charRace = prompt(charName + ', choose a race.  Human or Alien?').toUpperCase();
+console.log(charRace);
+
+if ((charRace === 'HUMAN') || (charRace === 'ALIEN')) {
+  alert(charName + ', You have selected the ' + charRace + ' race.');
+} else {
+  charRace = prompt('Sorry ' + charName + ', that was invalid, please try again.').toUpperCase();
+  console.log(charRace);
+}
+
+
+while ((charRace !== 'HUMAN') || (charRace !== 'ALIEN')) {
+  charRace = prompt(charName + ', you have selected an invalid character race, please try again.').toUpperCase();
+}
+alert(charName + ', you have selected the ' + charRace + ' Race.');
+
+alert('this is a test alert');
+*/
+// auto generate default race if they fail to type in the right answer
+
+
+
+
+
+
+
+
+// allow for scroll
